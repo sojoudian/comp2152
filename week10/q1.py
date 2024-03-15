665732349dd37414302c34d30e3835da35643dbd
Maziar Sojoudian <maziar@sojoudian.net>
2024-03-15 15:00:09 -0400

    update

--- week10/q1.py
+++
@@ -1,8 +0,0 @@
-def add_student(name, age, email):
-  conn = sqlite3.connect('school.db')
-  c = conn.cursor()
-  c.execute('''
-  INSERT INTO students (name, age, email) VALUES(?, ?, ?)
-  ''',(name, age, email))
-  conn.commit()
-  conn.close()
--- week10/q2.py
+++
@@ -1,7 +0,0 @@
-def get_students():
-  conn = sqlite3.connect('school.db')
-  c = conn.cursor()
-  c.execute('''SELECT * FROM students''')
-  students = c.fetchall()
-  conn.close()  
-  return students
